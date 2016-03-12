9aef317989331984bac882be933b2f2ef20ae025
Tony Bowden <tony@tmtm.com>
2016-03-12 08:25:07 +0000

    Back to 37th

--- scraper.rb
+++ scraper.rb
@@ -2,7 +2,6 @@
 # encoding: utf-8
 
 require 'wikidata/fetcher'
-require 'pry'
 
 @pages = [
   'Catégorie:Conseiller national suisse de la 49e législature',
@@ -14,9 +13,12 @@ require 'pry'
   'Catégorie:Conseiller national suisse de la 43e législature',
   'Catégorie:Conseiller national suisse de la 42e législature',
   'Catégorie:Conseiller national suisse de la 41e législature',
+  'Catégorie:Conseiller national suisse de la 40e législature',
+  'Catégorie:Conseiller national suisse de la 39e législature',
+  'Catégorie:Conseiller national suisse de la 38e législature',
+  'Catégorie:Conseiller national suisse de la 37e législature',
 ]
 
 names = @pages.map { |c| WikiData::Category.new(c, 'fr').member_titles }.flatten.uniq
 EveryPolitician::Wikidata.scrape_wikidata(names: { fr: names })
-warn EveryPolitician::Wikidata.notify_rebuilder
 
