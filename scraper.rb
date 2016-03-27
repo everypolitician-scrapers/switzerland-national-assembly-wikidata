cb4f7e34c6aebb36b878d3ebb0f9102650df84fd
Tony Bowden <tony@tmtm.com>
2016-03-27 22:17:19 +0100

    Add more historic data

--- scraper.rb
+++ scraper.rb
@@ -23,26 +23,31 @@ fr_names = @pages.map { |c| WikiData::Category.new(c, 'fr').member_titles }.flat
 
 
 de_names = {}
-de_names[50] = EveryPolitician::Wikidata.wikipedia_xpath( 
+de_names[50] = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://de.wikipedia.org/wiki/50._Legislaturperiode_der_Schweizer_Bundesversammlung',
   xpath: '//table[.//th[.="Name"]]//td[1]//a[not(@class="new")]/@title',
-) 
+)
 
-de_names[49] = EveryPolitician::Wikidata.wikipedia_xpath( 
+de_names[49] = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://de.wikipedia.org/wiki/49._Legislaturperiode_der_Schweizer_Bundesversammlung',
   xpath: '//table[.//th[.="Name"]]//td[1]//a[not(@class="new")]/@title',
-) 
+)
 
-de_names[48] = EveryPolitician::Wikidata.wikipedia_xpath( 
+de_names[48] = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://de.wikipedia.org/wiki/48._Legislaturperiode_der_Schweizer_Bundesversammlung',
   xpath: '//table[.//th[.="Name"]]//td[1]//a[not(@class="new")]/@title',
-) 
+)
 
-
-de_names[47] = EveryPolitician::Wikidata.wikipedia_xpath( 
+de_names[47] = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://de.wikipedia.org/wiki/47._Legislaturperiode_der_Schweizer_Bundesversammlung',
   xpath: '//table[.//th[.="Name"]]//td[1]//a[not(@class="new")]/@title',
-) 
+)
+
+de_names[45] = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://de.wikipedia.org/wiki/Schweizer_Parlamentswahlen_1995/Resultate_Nationalratswahlen',
+  xpath: '//table[.//th[.="Stimmen"]]//td[position() = last()]//a[not(@class="new")]/@title',
+  debug: true
+)
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { fr: fr_names, de: de_names.values.flatten.uniq }, batch_size: 100)
 
