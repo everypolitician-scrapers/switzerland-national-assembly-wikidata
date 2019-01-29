bbbd681f4ac399cdfe80377cb72504fd2c7696f7
Tony Bowden <tony@tmtm.com>
2019-01-29 07:58:18 +0000

    Only fetch via Wikidata
    There are no longer any members coming from Wikipedia who don't have
    Parliament IDs in Wikidata, so we can simplify to only find the latter.

--- scraper.rb
+++ scraper.rb
@@ -3,27 +3,8 @@
 
 require 'wikidata/fetcher'
 
-@pages = [
-  'Catégorie:Conseiller national suisse de la 50e législature',
-  'Catégorie:Conseiller national suisse de la 49e législature',
-  'Catégorie:Conseiller national suisse de la 48e législature',
-  'Catégorie:Conseiller national suisse de la 47e législature',
-  'Catégorie:Conseiller national suisse de la 46e législature',
-  'Catégorie:Conseiller national suisse de la 45e législature',
-  'Catégorie:Conseiller national suisse de la 44e législature',
-  'Catégorie:Conseiller national suisse de la 43e législature',
-  'Catégorie:Conseiller national suisse de la 42e législature',
-  'Catégorie:Conseiller national suisse de la 41e législature',
-  'Catégorie:Conseiller national suisse de la 40e législature',
-  'Catégorie:Conseiller national suisse de la 39e législature',
-  'Catégorie:Conseiller national suisse de la 38e législature',
-  'Catégorie:Conseiller national suisse de la 37e législature',
-]
-
-frwiki = @pages.map { |c| WikiData::Category.new(c, 'fr').wikidata_ids }.flatten.uniq
-
 # has Property: "Swiss Parliament ID"
-sparq = 'SELECT ?item WHERE { ?item wdt:P1307 ?id . }'
+sparq = 'SELECT ?item WHERE { ?item wdt:P1307 [] }'
 ids = EveryPolitician::Wikidata.sparql(sparq)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: frwiki | ids, batch_size: 250)
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, batch_size: 250)
